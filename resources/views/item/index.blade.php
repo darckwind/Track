9df100767dbea91abb67db4aa70a1f97ea81564d
franciscolagos <f.bunout01@ufromail.cl>
2019-11-26 15:45:48 -0300

    items

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -3,7 +3,29 @@
 @section('content')
 
 	<div class="container">
-		{{$item}}
+
+        <table class="table table-responsive-sm">
+            <tr>
+                <th>nombre instrumento</th>
+                <th>creado</th>
+                <th>Acction</th>
+            </tr>
+            @foreach($item as $items)
+                <tr>
+                    <td>{{$items->description }}</td>
+                    <td>{{$items->created_at}}</td>
+                    <td>
+                        <form action="{{ route('model.destroy',$items->id_item) }}" method="POST">
+                            <a class="btn btn-warning" href="{{ route('model.edit',$items->id_item) }}">edit</a>
+                            <!--selector multiples edicion de datos-->
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
 	</div>
 
 @endsection
