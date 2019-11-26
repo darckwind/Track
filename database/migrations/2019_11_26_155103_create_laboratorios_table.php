79236a718cedcd84462bcc6d56bb6dabf9affd13
franciscolagos <f.bunout01@ufromail.cl>
2019-11-26 18:00:34 -0300

    laboratorioa

--- database/migrations/2019_11_26_155103_create_laboratorios_table.php
+++ database/migrations/2019_11_26_155103_create_laboratorios_table.php
@@ -14,7 +14,7 @@ class CreateLaboratoriosTable extends Migration
     public function up()
     {
         Schema::create('laboratorios', function (Blueprint $table) {
-            $table->bigIncrements('id_lab');
+            $table->bigIncrements('id_laboratorio');
             $table->string('nombre_lab');
             $table->timestamps();
         });
