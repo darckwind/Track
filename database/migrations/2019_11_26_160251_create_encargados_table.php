cbe2d75f54a6be0b85c807d09440e9c8ba27730b
franciscolagos <f.bunout01@ufromail.cl>
2019-11-26 15:23:46 -0300

    correccion clave primaria

--- database/migrations/2019_11_26_160251_create_encargados_table.php
+++ database/migrations/2019_11_26_160251_create_encargados_table.php
@@ -14,13 +14,14 @@ class CreateEncargadosTable extends Migration
     public function up()
     {
         Schema::create('encargados', function (Blueprint $table) {
-            $table->bigIncrements('run_enc');
+            $table->string('run_enc');
             $table->string('nombre_enc');
             $table->string('apellido_enc');
             $table->string('telefono_enc');
             $table->string('email_enc');
             $table->unsignedBigInteger('id_tipo');
             $table->foreign('id_tipo')->references('id_tipo')->on('tipos')->onDelete('cascade');
+            $table->primary('run_enc');
             $table->timestamps();
         });
     }
