2d418b2e3dc04940a3396313be8a8e85d0a47d63
franciscolagos <f.bunout01@ufromail.cl>
2019-11-26 15:51:26 -0300

    items

--- app/item.php
+++ app/item.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class item extends Model
 {
-    //
+    protected $fillable = [
+        'id_item','descripcion'
+    ];
+    protected $primaryKey = 'id_item';
+
 }
