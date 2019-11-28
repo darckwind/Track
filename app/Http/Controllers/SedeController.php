7de509110acfa50a711c6d2ccfe9cccbf1a6618f
franciscolagos <f.bunout01@ufromail.cl>
2019-11-28 19:20:00 -0300

    seeder estados y desvinculacion con tipos

--- app/Http/Controllers/SedeController.php
+++ app/Http/Controllers/SedeController.php
@@ -17,11 +17,11 @@ class SedeController extends Controller
      */
     public function index()
     {
-        $sede = DB::table('sedes')
+        $sedes = DB::table('sedes')
             ->join('users', 'users.id', '=', 'sedes.id_users')
             ->select('sedes.id_sede', 'sedes.nombre_sede','sedes.direccion_sede','users.name')
             ->get();
-        return view('sede.index',compact('sede'));
+        return view('sede.index',compact('sedes'));
     }
 
     /**
