b4f1366022177863dac9e5b9670c3e92d23ed6d2
franciscolagos <f.bunout01@ufromail.cl>
2019-11-28 19:21:31 -0300

    seeder estados y desvinculacion con tipos

--- resources/views/sede/index.blade.php
+++ resources/views/sede/index.blade.php
@@ -29,7 +29,30 @@
             </div>
         @endif
 
-        {{$sedes}}
+        <table class="table table-responsive-sm">
+            <tr>
+                <th>Nombre Sede</th>
+                <th>Direccion</th>
+                <th>Encargado</th>
+                <th>Acction</th>
+            </tr>
+            @foreach($sede as $sedes)
+                <tr>
+                    <td>{{$sedes->nombre_sede}}</td>
+                    <td>{{$sedes->direccion_sede}}</td>
+                    <th>{{$sedes->name}}</th>
+                    <td>
+                        <form action="{{ route('sede.destroy',$sedes->id_sede) }}" method="POST">
+                            <a class="btn btn-warning" href="{{ route('sede.edit',$sedes->id_sede) }}">edit</a>
+                            <!--selector multiples edicion de datos-->
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
 	</div>
 
 @endsection
