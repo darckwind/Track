81f2f36b3b45027af7341dcb9ddedad31b9c099c
franciscolagos <f.bunout01@ufromail.cl>
2019-11-28 19:28:37 -0300

    seeder estados y desvinculacion con tipos

--- app/Http/Controllers/SedeController.php
+++ app/Http/Controllers/SedeController.php
@@ -57,7 +57,7 @@ class SedeController extends Controller
         $sede->id_estado = $request->input('estado');
         $sede->save();
 
-        $sede = DB::table('sedes')
+        $sedes = DB::table('sedes')
             ->join('users', 'users.id', '=', 'sedes.id_users')
             ->select('sedes.id_sede', 'sedes.nombre_sede','sedes.direccion_sede','users.name')
             ->get();
