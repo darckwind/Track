bb3e06269e4c1d92cc7bacb9c4f5f5c6e3b2a6e1
franciscolagos <f.bunout01@ufromail.cl>
2019-12-02 15:11:07 -0300

    index envios lab

--- resources/views/env_lab/index.blade.php
+++ resources/views/env_lab/index.blade.php
@@ -2,6 +2,31 @@
 
 @section('content')
 
-    holi
+    <div class="container">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Vista general Laboratorios</h2>
+            </div>
+        </div>
+        <table class="table table-responsive-sm">
+            <tr>
+                <th>Laboratorio</th>
+                <th>Instrumento</th>
+                <th>Cantidad</th>
+                <th>Creado en</th>
+                <th>Accion</th>
+            </tr>
+
+            <tr>
+                <td>s</td>
+                <td>s</td>
+                <td>s</td>
+                <td>s</td>
+                <td>
+                    1
+                </td>
+            </tr>
+        </table>
+    </div>
 
 @endsection
