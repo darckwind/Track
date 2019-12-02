01d89c0e7cc0359b8ce566ff75652622a69d6651
franciscolagos <f.bunout01@ufromail.cl>
2019-12-02 15:39:52 -0300

    index envios lab

--- resources/views/env_lab/create.blade.php
+++ resources/views/env_lab/create.blade.php
@@ -2,8 +2,31 @@
 
 @section('content')
     <div class="container">
-        {{$sede}}
-        {{$lab}}
-        {{$item}}
+        <div class="row">
+            <div class="col-md-6">
+                <select name="sede" class="form-control">
+                    @foreach($sede as $sedes)
+                        <option value="{{$sedes->id_sede}}">{{$sedes->nombre_sede}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-md-6">
+                <select name="lab" class="form-control">
+                    @foreach($lab as $labs)
+                        <option value="{{$labs->id_laboratorio}}">{{$labs->nombre_lab}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-md-6">
+                <select name="item" class="form-control">
+                    @foreach($item as $items)
+                        <option value="{{$items->id_item}}">{{$items->description}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-md-6">
+                <input type="number" class="form-control" name="cantidad" min="0">
+            </div>
+        </div>
     </div>
 @endsection
