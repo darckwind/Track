9354f61cdbe58230d3cffb4bf60eb455dbc3e5c6
franciscolagos <f.bunout01@ufromail.cl>
2019-12-03 17:26:33 -0300

    index envios lab

--- app/Http/Controllers/EnvioLabController.php
+++ app/Http/Controllers/EnvioLabController.php
@@ -109,7 +109,7 @@ class EnvioLabController extends Controller
      */
     public function destroy(envio_lab $envio_lab)
     {
-        dd($envio_lab);
+        die($envio_lab);
 
         $enviado = DB::table('envio_labs')
             ->join('laboratorios', 'laboratorios.id_laboratorio', '=', 'envio_labs.id_laboratorio')
