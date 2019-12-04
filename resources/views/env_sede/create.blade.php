2d47d0e964fd4b6327e317dfdf850985a1d63da4
franciscolagos <f.bunout01@ufromail.cl>
2019-12-03 23:43:41 -0300

    index envios lab

--- resources/views/env_sede/create.blade.php
+++ resources/views/env_sede/create.blade.php
@@ -7,7 +7,22 @@
         <form action="{{ route('envsede.store') }}" method="POST">
             @csrf
             <div class="row">
-
+                <div class="col-md-6">
+                    <label for="exampleInputEmail1">Sede</label>
+                    <select name="lab" class="form-control">
+                        @foreach($sede as $sedes)
+                            <option value="{{$sedes->id_sede}}">{{$labs->nombre_sede}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-md-6">
+                    <label for="exampleInputEmail1">Laboratorio</label>
+                    <select name="lab" class="form-control">
+                        @foreach($lab as $labs)
+                            <option value="{{$labs->id_laboratorio}}">{{$labs->nombre_lab}}</option>
+                        @endforeach
+                    </select>
+                </div>
             </div>
         </form>
     </div>
