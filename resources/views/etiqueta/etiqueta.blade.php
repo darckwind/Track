494ba561fc5d8f4d6f303e879b2d27d781ac2062
franciscolagos <f.bunout01@ufromail.cl>
2019-12-04 17:18:21 -0300

    creacion centros

--- resources/views/etiqueta/etiqueta.blade.php
+++ resources/views/etiqueta/etiqueta.blade.php
@@ -1,8 +1,21 @@
 @foreach($lab as $labs)
-    <table border="1">
+    <table>
         <tr>
             <td>Laboratorio</td>
             <td>{{$labs->nombre_lab}}</td>
         </tr>
     </table>
+    <table>
+        <tr>
+            <th>Instrumento</th>
+            <th>cantidad</th>
+        </tr>
+        @foreach($lab as $labss)
+            <tr>
+                <td>{{$labss->description}}</td>
+                <td>{{$labss->cantidad}}</td>
+            </tr>
+        @endforeach
+    </table>
 @endforeach
+
