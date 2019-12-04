efe9a618b786fdbf87bcc968d598a3ab87f1ef54
franciscolagos <f.bunout01@ufromail.cl>
2019-12-04 18:36:04 -0300

    creacion centros

--- resources/views/etiqueta/etiqueta.blade.php
+++ resources/views/etiqueta/etiqueta.blade.php
@@ -3,8 +3,11 @@
     border-collapse: collapse;
     }
 
-    div,table, th, td {
+    table, th, td {
     border: 1px solid black;
+    }
+    .col-md-6{
+        border: 1px solid black;
     }
 </style>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
