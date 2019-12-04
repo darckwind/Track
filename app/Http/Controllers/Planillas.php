730f97bb746e14e89b9013cb63c2b6bd91184478
franciscolagos <f.bunout01@ufromail.cl>
2019-12-04 17:01:10 -0300

    creacion centros

--- app/Http/Controllers/Planillas.php
+++ app/Http/Controllers/Planillas.php
@@ -2,14 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\envio_lab;
 use App\laboratorio;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class Planillas extends Controller
 {
     public function laboratorio()
     {
-        $lab = laboratorio::all();
+        $lab = DB::table('envio_labs')
+            ->join('laboratorios', 'laboratorios.id_laboratorio', '=', 'envio_labs.id_laboratorio')
+            ->join('items', 'items.id_item', '=', 'envio_labs.id_item')
+            ->select('envio_labs.id_env_lab','envio_labs.cantidad', 'items.description','laboratorios.nombre_lab')
+            ->get();
+
         return view('etiqueta.etiqueta',compact('lab'));
     }
 }
