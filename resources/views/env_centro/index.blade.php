19e67cd321b1e7f794b8875ccc1623d95917dae7
franciscolagos <f.bunout01@ufromail.cl>
2019-12-05 10:33:21 -0300

    creacion centros

--- resources/views/env_centro/index.blade.php
+++ resources/views/env_centro/index.blade.php
@@ -2,6 +2,33 @@
 
 @section('content')
     <div class="container">
-        {{$enviado}}
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Vista general envios Sedes</h2>
+            </div>
+        </div>
+        <table class="table table-responsive-sm">
+            <tr>
+                <th>Centro</th>
+                <th>Sede</th>
+                <th>Accion</th>
+            </tr>
+            @foreach($enviado as $envs)
+                <tr>
+                    <td>{{$envs->nombre_centro}}</td>
+                    <td>{{$envs->nombre_sede}}</td>
+
+                    <td>
+                        <form action="{{ route('envsede.destroy',$envs->id_env_cen) }}" method="POST">
+                            <a class="btn btn-warning" href="{{ route('envsede.edit',$envs->id_env_cen) }}">edit</a>
+                            <!--selector multiples edicion de datos-->
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
     </div>
 @endsection
