e8aeffa1c818763897787ac6b6d5e08728334600
franciscolagos <f.bunout01@ufromail.cl>
2019-12-05 10:54:15 -0300

    creacion centros

--- app/envio_centro.php
+++ app/envio_centro.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class envio_centro extends Model
 {
-    //
+    protected $fillable = [
+        'id_sede','id_centro'
+    ];
+    protected $primaryKey = 'id_env_cen';
 }
