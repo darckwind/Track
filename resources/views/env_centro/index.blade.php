fffd43085fe2935b5d3cf0d2ac25f19a52b04fec
franciscolagos <f.bunout01@ufromail.cl>
2019-12-07 03:29:09 -0300

    redireccion a home

--- resources/views/env_centro/index.blade.php
+++ resources/views/env_centro/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Vista general envios Sedes</h2>
+                <h2>Vista general envios Centros</h2>
             </div>
         </div>
         <table class="table table-responsive-sm">
