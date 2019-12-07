56aaf1aa7096a26e92d61d83554925aeddefad43
franciscolagos <f.bunout01@ufromail.cl>
2019-12-07 03:58:53 -0300

    redireccion a home

--- resources/views/etiqueta/centro.blade.php
+++ resources/views/etiqueta/centro.blade.php
@@ -17,7 +17,7 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 
-{{$enviado}}
+{{$env}}
 <div class="row">
     @foreach($centro as $centros)
         <div class="col-md-6 row">
@@ -67,7 +67,7 @@
                 </table>
             </div>
             <div class="col-md-12">
-                @foreach($enviado as $env_cen)
+                @foreach($env as $env_cen)
                     @if($env_cen->id_centro == $centros->id_centro)
                         {{$env_cen}}
                     @endif
