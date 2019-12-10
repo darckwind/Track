98f0d13b6ad39c5d2ab06b65b7ca6712bd54d692
franciscolagos <f.bunout01@ufromail.cl>
2019-12-10 16:40:52 -0300

    redireccion a home

--- resources/views/env_lab/create.blade.php
+++ resources/views/env_lab/create.blade.php
@@ -7,6 +7,7 @@
                 @csrf
                 <div class="col-md-4">
                     <label for="exampleInputEmail1">Laboratorio</label>
+                    <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Busqueda" class="col-sm-3 filter-table">
                     <select name="lab" class="form-control">
                         @foreach($lab as $labs)
                             <option value="{{$labs->id_laboratorio}}">{{$labs->nombre_lab}}</option>
@@ -32,4 +33,30 @@
             </div>
         </form>
     </div>
+
+
+
+
+    <script>
+        function myFunction() {
+            // Declare variables
+            var input, filter, x, tr, td, i, txtValue;
+            input = document.getElementById("myInput");
+            filter = input.value.toUpperCase();
+            x = document.getElementsByName("lab");
+
+
+            // Loop through all table rows, and hide those who don't match the search query
+            for(let i = 0; i < x[0].getElementsByTagName("option").length; i++){
+
+                if(x[0].getElementsByTagName("option")[i].innerText.toUpperCase().indexOf(filter)>-1){
+                    x[0].getElementsByTagName("option")[i].style.display="";
+                }else{
+                    x[0].getElementsByTagName("option")[i].style.display="none";
+
+                }
+
+            }
+        }
+    </script>
 @endsection
--- resources/views/env_sede/create.blade.php
+++ resources/views/env_sede/create.blade.php
@@ -8,7 +8,7 @@
             <div class="row">
                 <div class="col-md-6">
                     <label for="exampleInputEmail1">Sede</label>
-                    <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Busqueda" class="col-sm-3 filter-table">
+
                     <select name="sede" class="form-control">
                         @foreach($sede as $sedes)
                             <option value="{{$sedes->id_sede}}">{{$sedes->nombre_sede}}</option>
@@ -30,28 +30,7 @@
         </form>
     </div>
 
-    <script>
-        function myFunction() {
-            // Declare variables
-            var input, filter, x, tr, td, i, txtValue;
-            input = document.getElementById("myInput");
-            filter = input.value.toUpperCase();
-            x = document.getElementsByName("lab");
 
-
-            // Loop through all table rows, and hide those who don't match the search query
-            for(let i = 0; i < x[0].getElementsByTagName("option").length; i++){
-
-                if(x[0].getElementsByTagName("option")[i].innerText.toUpperCase().indexOf(filter)>-1){
-                    x[0].getElementsByTagName("option")[i].style.display="";
-                }else{
-                    x[0].getElementsByTagName("option")[i].style.display="none";
-
-                }
-
-            }
-        }
-    </script>
 
 
 
